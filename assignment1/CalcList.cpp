cbfc288d843d31d644440aad0faea2fb6ab495f2
Rylan <rylanpietras@gmail.com>
2024-09-27 00:37:18 -0400

    CalcList Definitions 1

--- assignment1/CalcList.cpp
+++ assignment1/CalcList.cpp
@@ -1,7 +1,97 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "CalcList.hpp"
 
+
 double CalcList :: total() const {
+    return top->totalVal;
+}
+
+void CalcList :: newOperation(const FUNCTIONS func, const double operand) {
+    if (top == nullptr) { //first if statement for first Node
+        Node *temp = new Node;
+
+        switch(func) { // temp->totalVal just accesses the default values for Node
+            case ADDITION:
+                temp->totalVal = temp->totalVal + operand;
+                break;
+            case SUBTRACTION:
+                temp->totalVal = temp->totalVal - operand;
+                break;
+            case MULTIPLICATION:
+                temp->totalVal = temp->totalVal * operand;
+                break;
+            case DIVISION:
+                if(operand == 0){
+                    throw("Divide by 0 Error");
+                }
+                else{
+                    temp->totalVal = temp->totalVal / operand;
+                }   
+        }
+
+        temp->newOp = func;
+        temp->newNum = operand;
+        temp->numOfOps = (temp->numOfOps)++;
+        temp->next = nullptr;
+
+        top = temp;
+    }
+    else{ //else for any node other than first
+        Node *temp = new Node;
+
+        switch(func) {
+            case ADDITION:
+                temp->totalVal = top->totalVal + operand;
+                break;
+            case SUBTRACTION:
+                temp->totalVal = top->totalVal - operand;
+                break;
+            case MULTIPLICATION:
+                temp->totalVal = top->totalVal * operand;
+                break;
+            case DIVISION:
+                if (operand == 0) {
+                    throw("Divide by 0 Error");
+                }
+                else {
+                    temp->totalVal = top->totalVal / operand;
+                }
+        }
+
+        temp->newOp = func;
+        temp->newNum = operand;
+        temp->numOfOps = (top->numOfOps)++;
+        temp->next = top;
+
+        top = temp;
+    }
+}
+
+void CalcList :: removeLastOperation() {
+    Node *temp = top;
+    top = top->next;
+    delete temp;
+}
+
+// toString function in progress
+std::string CalcList :: toString(unsigned short precision) const {
+    if(precision < 0 || precision > 10) {
+        throw("Precision Input Invalid");
+    }
+
+    std::string allOutputs = "";
     
+    Node *temp = top;
+    while (temp != nullptr) {
+
+        temp = temp->next;
+    }
+
+    return allOutputs;
+}
+
+int main() {
+    return 0;
 }
--- assignment1/CalcList.hpp
+++ assignment1/CalcList.hpp
@@ -5,6 +5,7 @@
 
 class CalcList : public CalcListInterface {
 public:
+    CalcList() { ; }
     double total() const;
     void newOperation(const FUNCTIONS func, const double operand);
     void removeLastOperation();
@@ -13,11 +14,11 @@ public:
 private:
     class Node {
     public:
-        double total = 0;
+        double totalVal = 0;
         FUNCTIONS newOp = ADDITION;
         double newNum = 0;
+        int numOfOps = 0;
         Node *next = nullptr;
-        int numOps = 0;
     };
 
     Node *top = nullptr;
