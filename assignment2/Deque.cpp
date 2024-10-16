0508487af448fd11e9d40456c11d8257481b0df8
Rylan <rylanpietras@gmail.com>
2024-10-16 00:16:50 -0400

    toString function and Deque constructor finished

--- assignment2/Deque.cpp
+++ assignment2/Deque.cpp
@@ -3,50 +3,49 @@
 
 #include "Deque.hpp"
 
-class DequeEmpty : std::runtime_error
-{
+class DequeEmpty : std::runtime_error {
 public:
     DequeEmpty() : std::runtime_error("Deque is empty") {};
 };
 
+Deque::Deque(std::string inStr) {
+    int strLen = inStr.length();
+
+    for (int i = 0; i < strLen; i++) {
+        pushBack(inStr[i]);
+    }
+}
+
 // Return current front of the deque
-char Deque::front() const
-{
-    if (isEmpty())
-    {
+char Deque::front() const {
+    if (isEmpty()) {
         throw DequeEmpty();
     }
-    else
-    {
+    else {
         return head->ch;
     }
 }
 
 // Return current back of the deque
-char Deque::back() const
-{
-    if (isEmpty())
-    {
+char Deque::back() const {
+    if (isEmpty()) {
         throw DequeEmpty();
     }
-    else
-    {
+    else {
         return tail->ch;
     }
 }
 
 // Push new node to front of deque
-void Deque::pushFront(char ch)
-{
+void Deque::pushFront(char ch) {
     Node *n = new Node;
+    n->ch = ch;
     // If first node in deque
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         head = n;
         tail = n;
     }
-    else
-    {
+    else {
         Node *thead = head;
         n->next = head;
         head = n;
@@ -56,17 +55,15 @@ void Deque::pushFront(char ch)
 }
 
 // Push new node to back of deque
-void Deque::pushBack(char ch)
-{
+void Deque::pushBack(char ch) {
     Node *n = new Node;
+    n->ch = ch;
     // If first node in deque
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         head = n;
         tail = n;
     }
-    else
-    {
+    else {
         Node *ttail = tail;
         n->prev = tail;
         tail = n;
@@ -76,15 +73,18 @@ void Deque::pushBack(char ch)
 }
 
 // Pop first node from deque
-char Deque::popFront()
-{
+char Deque::popFront() {
     char ch = ' ';
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         throw DequeEmpty();
     }
-    else
-    {
+    else if (head == tail) { // If there is only one node left
+        ch = head->ch;
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+    }
+    else {
         ch = head->ch;
         Node *old = head;
         head = head->next;
@@ -95,15 +95,18 @@ char Deque::popFront()
 }
 
 // Pop last node from deque
-char Deque::popBack()
-{
+char Deque::popBack() {
     char ch = ' ';
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         throw DequeEmpty();
     }
-    else
-    {
+    else if (tail == head) { // If there is only one node left
+        ch = tail->ch;
+        delete tail;
+        head = nullptr;
+        tail = nullptr;
+    }
+    else {
         ch = tail->ch;
         Node *old = tail;
         tail = tail->prev;
@@ -114,9 +117,38 @@ char Deque::popBack()
 }
 
 // Check if deque is empty
-bool Deque::isEmpty() const
-{
+bool Deque::isEmpty() const {
     if (head == nullptr && tail == nullptr)
         return true;
     return false;
+}
+
+std::string Deque::toString() const {
+    Node *temp = head;
+    std::string outStr = "";
+
+    while (temp != nullptr) {
+        outStr += temp->ch;
+        temp = temp->next;
+    }
+
+    return outStr;
+}
+
+int main() { // main function just for testing (remove before submission)
+    Deque d1;
+    
+    d1.pushBack('h');
+    d1.pushBack('e');
+    d1.pushBack('l');
+    d1.pushBack('l');
+    d1.pushBack('o');
+
+    std::cout << d1.toString() << std::endl;
+
+    Deque d2("goodbye");
+
+    std::cout << d2.toString() << std::endl;
+
+    return 0;
 }
