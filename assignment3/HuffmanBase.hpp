935d1b319cfe9b4f4f6253d98f334f07de21409a
Michelle McAveety <mcaveetym@usf.edu>
2024-11-02 22:01:53 -0400

    WE BE COOKINgit status!

--- assignment3/HuffmanBase.hpp
+++ assignment3/HuffmanBase.hpp
@@ -4,43 +4,48 @@
 #include <cstddef>
 #include <string>
 #include <iostream>
+#include <map>
+
+#include "HuffmanTree.hpp"
+#include "HuffmanBase.hpp"
 
 class HuffmanNode {
-public:
-  HuffmanNode(char c, size_t f, HuffmanNode *p, HuffmanNode *l, HuffmanNode *r) : character(c), frequency(f), parent(p), left(l), right(r) {};
-  HuffmanNode(char c, size_t f) : HuffmanNode(c, f, nullptr, nullptr, nullptr) {};
-
-  char getCharacter() const;
-  size_t getFrequency() const;
-
-  bool isLeaf() const;
-  bool isBranch() const;
-  bool isRoot() const;
-
-  class Compare {
   public:
-    Compare(bool lessThan = true) : lessThan(lessThan) {};
-    bool operator()(const HuffmanNode &n1, const HuffmanNode &n2) const;
-    bool operator()(const HuffmanNode *n1, const HuffmanNode *n2) const;
+    HuffmanNode(char c, size_t f, HuffmanNode *p, HuffmanNode *l, HuffmanNode *r) : character(c), frequency(f), parent(p), left(l), right(r) {};
+    HuffmanNode(char c, size_t f) : HuffmanNode(c, f, nullptr, nullptr, nullptr) {};
+    HuffmanNode() {};
+
+    char getCharacter() const;
+    size_t getFrequency() const;
+
+    bool isLeaf() const;
+    bool isBranch() const;
+    bool isRoot() const;
+
+    class Compare {
+    public:
+      Compare(bool lessThan = true) : lessThan(lessThan) {};
+      bool operator()(const HuffmanNode &n1, const HuffmanNode &n2) const;
+      bool operator()(const HuffmanNode *n1, const HuffmanNode *n2) const;
+    private:
+      bool lessThan;
+    };
+
   private:
-    bool lessThan;
-  };
+    char character;
+    size_t frequency;
 
-private:
-  char character;
-  size_t frequency;
-
-public: 
-  HuffmanNode *parent;
-  HuffmanNode *left;
-  HuffmanNode *right;
+  public: 
+    HuffmanNode *parent;
+    HuffmanNode *left;
+    HuffmanNode *right;
 };
 
 class HuffmanTreeBase {
   public:
-  virtual std::string compress(const std::string inputStr) = 0;
-  virtual std::string serializeTree() const = 0;
-  virtual std::string decompress(const std::string inputCode, const std::string serializedTree) = 0;
+    virtual std::string compress(const std::string inputStr) = 0;
+    virtual std::string serializeTree() const = 0;
+    virtual std::string decompress(const std::string inputCode, const std::string serializedTree) = 0;
 };
 
 #endif /* HUFFMANBASE_H */
--- assignment3/HuffmanTree.cpp
+++ assignment3/HuffmanTree.cpp
@@ -1,14 +1,20 @@
+#include <cstddef>
+#include <string>
 #include <iostream>
 #include <map>
+
 #include "HuffmanTree.hpp"
+#include "HuffmanBase.hpp"
+#include "HeapQueue.hpp"
 
 std::map<char, int> HuffmanTree::giveFreqMap(std::string str) {
     std::map<char, int> freqMap;
     std::map<char, int>::iterator it;
     int len = str.length();
 
+    // Count character frequency in string
     for (int i = 0; i < len; ++i) {
-        freqMap.find(str[i]) = it;
+        it = freqMap.find(str[i]);
 
         if (it != freqMap.end()) {
             freqMap[str[i]] = ++(it->second);
@@ -21,9 +27,50 @@ std::map<char, int> HuffmanTree::giveFreqMap(std::string str) {
     return freqMap;
 }
 
-// int main() {
+std::string HuffmanTree::compress(const std::string inputStr){
 
-//     // this is a test
+    HuffmanTree tree;
+    std::map<char, int> freqmap = tree.giveFreqMap(inputStr);
+    std::map<char,int>::iterator i;
+    HeapQueue<HuffmanNode, HuffmanNode::Compare> minHeap;
 
-//     return 0;
-// }
+    // For each item in the frequency map, add a node to the minheap
+    for(i = freqmap.begin(); i != freqmap.end(); i++){
+        std::cout << i->first << ": " << i->second << std::endl;
+
+        // Create temporary node
+        HuffmanNode temp(i->first, i->second);
+
+        minHeap.insert(temp);
+    }
+
+    // Remove each minimum and print
+    std::cout << "Highest priority in minheap" << std::endl;
+    int length = minHeap.size();
+    for(int i = 0; i < length; i++){
+        HuffmanNode temp = minHeap.min();
+        minHeap.removeMin();
+        std::cout << temp.getCharacter() << ": " << temp.getFrequency() << std::endl;
+    }
+    std::cout << "Minheap is now empty" << std::endl;
+
+    return "yay";
+
+}
+
+std::string HuffmanTree::decompress(const std::string inputCode, const std::string serializedTree){
+    return "testing";
+}
+std::string HuffmanTree::serializeTree() const{
+    return "testing";
+}
+
+// Main function (remove before submission or test)
+int main() {
+
+    std::string test = "The quick brown fox jumped over the lazy dog.";
+    HuffmanTree t;
+    t.compress(test);
+
+    return 0;
+}
