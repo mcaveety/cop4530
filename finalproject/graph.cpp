06195474295d8e12eaa8ab9f86a27974098600a5
Rylan <rylanpietras@gmail.com>
2024-11-19 17:12:41 -0500

    starting stuff

--- finalproject/graph.cpp
+++ finalproject/graph.cpp
@@ -1,1 +1,18 @@
-// Group-defined graph.cpp code
+#include <iostream>
+#include "graph.hpp"
+
+void AdjList::addVertex(std::string label) {
+    class Vert temp(label);
+    graph.push_back(temp);
+}
+
+void AdjList::removeVertex(std::string label) {
+    std::vector<Vert>::iterator it;
+    it = graph.begin();
+    
+    while (it != graph.end()) {
+        if (it->name == label) {
+            graph.erase(it);
+        }
+    }
+}
--- finalproject/graph.hpp
+++ finalproject/graph.hpp
@@ -1,1 +1,42 @@
-// Group-defined graph.hpp header file
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include "graphbase.hpp"
+
+class AdjList {
+private:
+    class Vert {
+    public:
+        Vert(std::string str) { name = str; }
+        std::string name;
+        bool explored;
+        int distance = 999999;
+        Vert *lastVisted = nullptr;
+    };
+
+    class Neighbor {
+    public:
+        Neighbor();
+        char name;
+        int weight;
+        Neighbor *next = nullptr;
+    };
+
+    std::vector<Vert> graph;
+    int nextVertIndex = 0;
+
+public:
+    void addVertex(std::string label);
+
+    void removeVertex(std::string label);
+
+    void addEdge(std::string label1, std::string label2, unsigned long weight);
+
+    void removeEdge(std::string label1, std::string label2);
+
+    unsigned long shortestPath(std::string startLabel, std::string endLabel,
+    std::vector<std::string> &path);
+};
+
+#endif
