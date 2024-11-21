34d3bd5b24f66b49b468367b4162d777242d75af
Gl1tchingV1rus <146281717+Gl1tchingV1rus@users.noreply.github.com>
2024-11-20 20:13:38 -0500

    Expanded RemoveVertex
    Needs to be trimmed and some sent to RemoveEdge

--- finalproject/graph.cpp
+++ finalproject/graph.cpp
@@ -1,1 +1,51 @@
-// Group-defined graph.cpp code
+#include <iostream>
+#include "graph.hpp"
+
+void AdjList::addVertex(std::string label) {
+    class Vert temp(label);
+    graph.push_back(temp);
+}
+
+void AdjList::removeEdge(std::string label, std::string label2){
+
+}
+
+void AdjList::removeVertex(std::string label) {
+    std::vector<Vert>::iterator it;
+    it = graph.begin();
+    
+    while (it != graph.end()) {
+        /*if (it->name == label) {
+            graph.erase(it);
+            return;
+        }*/
+        AdjList::Neighbor *next_neighbor = it->first;
+        AdjList::Neighbor *prev_neighbor = nullptr;
+        while (next_neighbor != nullptr){
+            if (next_neighbor->name == label){
+                if (next_neighbor == it->first){ 
+                    it->first = next_neighbor->next;
+                    delete next_neighbor;
+                    break;
+                }
+
+                prev_neighbor->next = next_neighbor->next;
+                delete next_neighbor;
+                break;
+            }
+            prev_neighbor = next_neighbor;
+            next_neighbor = next_neighbor->next;
+        }
+
+        if (it->name == label){
+            while (it->first != nullptr){
+                AdjList::Neighbor* temp = it->first; // A -> B -> C -> D
+                it->first = it->first->next;
+                delete temp;
+            }
+            graph.erase(it);
+        }
+
+        it++;
+    }
+}
--- finalproject/graph.hpp
+++ finalproject/graph.hpp
@@ -1,1 +1,56 @@
-// Group-defined graph.hpp header file
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include "graphbase.hpp"
+
+class AdjList {
+private:
+    /*class Vert {
+    public:
+        Vert(std::string str) { name = str; }
+        std::string name;
+        bool explored = false;
+        int distance = 999999;
+        Vert *lastVisted = nullptr;
+        Neighbor* first;
+    };*/
+
+    class Neighbor {
+    public:
+        std::string name;
+        int weight;
+        Neighbor *next = nullptr;
+        Neighbor() {
+            std::string name;
+            weight;
+            Neighbor *next = nullptr;
+        }
+    };
+    class Vert { 
+    public:
+        Vert(std::string str) { name = str; }
+        std::string name;
+        bool explored = false;
+        int distance = 999999;
+        Vert *lastVisted = nullptr;
+        Neighbor* first = nullptr;
+    };
+
+    std::vector<Vert> graph;
+    int nextVertIndex = 0;
+
+public:
+    void addVertex(std::string label);
+
+    void removeVertex(std::string label);
+
+    void addEdge(std::string label1, std::string label2, unsigned long weight);
+
+    void removeEdge(std::string label1, std::string label2);
+
+    unsigned long shortestPath(std::string startLabel, std::string endLabel,
+    std::vector<std::string> &path);
+};
+
+#endif
