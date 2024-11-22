4b957a1b6928af4280dc81b270d8c065e97d4445
Gl1tchingV1rus <146281717+Gl1tchingV1rus@users.noreply.github.com>
2024-11-22 15:42:15 -0500

    Did some more testing

--- finalproject/main.cpp
+++ finalproject/main.cpp
@@ -40,25 +40,27 @@ int main() {
     // std::vector<std::string> vertices1 = {"1","2","3","4","5","6"};
     // std::vector<std::tuple<std::string, std::string, unsigned long>> edges1 = {{"1","2",7}, {"1","3",9}, {"1","6",14}, {"2","3",10}, {"2","4",15}, {"3","4",11}, {"3","6",2}, {"4","5",6}, {"5", "6", 9}};
 
-    g1.addVertex("1");
-    g1.addVertex("2");
-    g1.addVertex("3");
-    g1.addVertex("4");
-    g1.addVertex("5");
-    g1.addVertex("6");
+    g1.addVertex("A");
+    g1.addVertex("B");
+    g1.addVertex("C");
+    g1.addVertex("D");
+    g1.addVertex("E");
+    g1.addVertex("F");
+    g1.addVertex("G");
+    g1.addVertex("H");
 
-    g1.addEdge("1", "2", 7);
-    g1.addEdge("1", "3", 9);
-    g1.addEdge("1", "6", 14);
-    g1.addEdge("2", "3", 10);
-    g1.addEdge("2", "4", 15);
-    g1.addEdge("3", "4", 11);
-    g1.addEdge("3", "6", 2);
-    g1.addEdge("4", "5", 6);
-    g1.addEdge("5", "6", 9);
+    g1.addEdge("A", "B", 1);
+    g1.addEdge("B", "E", 3);
+    g1.addEdge("A", "C", 10);
+    g1.addEdge("C", "D", 7);
+    g1.addEdge("E", "D", 2);
+    g1.addEdge("D", "F", 3);
+    g1.addEdge("D", "G", 8);
+    g1.addEdge("F", "G", 2);
+    g1.addEdge("G", "H", 1);
 
     std::vector<std::string> path;
-    unsigned long shortestDist = g1.shortestPath("1", "5", path);
+    unsigned long shortestDist = g1.shortestPath("A", "C", path);
     std::vector<std::string>::iterator pathIt = path.begin();
     std::cout << "Shortest path to end is: " << shortestDist << "\n";
     while(pathIt != path.end()){
